ecec54d34072024c60f5923cf2975840f9b91e73
Rachel Wong <rachel.wong62@gmail.com>
2025-12-01 17:52:04 +0100

    Day 1

--- 2025/solutions/lib/day01.ex
+++ 2025/solutions/lib/day01.ex
@@ -1,5 +1,49 @@
 defmodule Day01 do
-  def run(contents) do
-    contents
+  defp rotate(location, direction, distance) do
+    cond do
+      direction == "L" -> location - distance
+      direction == "R" -> location + distance
+    end
+    |> Integer.mod(100)
+  end
+
+  defp final_crossing(location, direction, distance) do
+    remainder = Integer.mod(distance, 100)
+
+    cond do
+      location == 0 -> 0
+      direction == "L" and location <= remainder -> 1
+      direction == "R" and 100 - location <= remainder -> 1
+      true -> 0
+    end
+  end
+
+  defp count_final_zeroes(command, {location, count}) do
+    <<direction::binary-size(1), distance::binary>> = command
+    distance = String.to_integer(distance)
+
+    location = rotate(location, direction, distance)
+    count = if location == 0, do: count + 1, else: count
+    {location, count}
+  end
+
+  defp count_all_zeroes(command, {location, count}) do
+    <<direction::binary-size(1), distance::binary>> = command
+    distance = String.to_integer(distance)
+
+    count =
+      count + Integer.floor_div(distance, 100) + final_crossing(location, direction, distance)
+
+    location = rotate(location, direction, distance)
+
+    {location, count}
+  end
+
+  def part1(contents) do
+    contents |> String.split("\n") |> Enum.reduce({50, 0}, &count_final_zeroes/2) |> elem(1)
+  end
+
+  def part2(contents) do
+    contents |> String.split("\n") |> Enum.reduce({50, 0}, &count_all_zeroes/2) |> elem(1)
   end
 end
--- 2025/solutions/lib/solutions.ex
+++ 2025/solutions/lib/solutions.ex
@@ -1,19 +1,23 @@
 defmodule Solutions do
-  defp _run(day, prefix) do
+  defp _run(day, part, prefix) do
     suffix = day |> Integer.to_string() |> String.pad_leading(2, "0")
 
     {:ok, contents} =
       __DIR__ |> Path.dirname() |> Path.join("input/#{prefix}#{suffix}.txt") |> File.read()
 
     module = Module.concat(["Day#{suffix}" |> String.to_atom()])
-    apply(module, :run, [contents])
+
+    cond do
+      part == 1 -> apply(module, :part1, [contents])
+      part == 2 -> apply(module, :part2, [contents])
+    end
   end
 
-  def test(day) do
-    _run(day, "test")
+  def test(day, part) do
+    _run(day, part, "test")
   end
 
-  def run(day) do
-    _run(day, "day")
+  def run(day, part) do
+    _run(day, part, "day")
   end
 end
